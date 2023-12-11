be8686a42193c9eb399692d3ca65ccf4f25f7435
mlsmoorman <melissa.s.moorman@gmail.com>
2023-12-11 15:31:41 -0600

    lap pt2 complete - adds index and detail

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('books/', views.books_index, name='index'),
+    path('cats/<int:book_id>/', views.books_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from .models import Book
 
-books = [
-    {'name': "Beginner's Step-By-Step Coding Course", 'author': 'DK', 'description': "Written by a team of expert coders and coding teachers, Beginner's Step-by-Step Coding Course is the ideal way to get to set you on the road to code."},
-    {'name': "Everything You Need to ACE Computer Science and Coding", 'author': 'Workman Publishing', 'description': "This Big Fat Notebook makes it all “sink in” with key concepts, mnemonic devices, definitions, diagrams, and doodles to help you understand computer science."},
-]
+# books = [
+#     {'name': "Beginner's Step-By-Step Coding Course", 'author': 'DK', 'description': "Written by a team of expert coders and coding teachers, Beginner's Step-by-Step Coding Course is the ideal way to get to set you on the road to code."},
+#     {'name': "Everything You Need to ACE Computer Science and Coding", 'author': 'Workman Publishing', 'description': "This Big Fat Notebook makes it all “sink in” with key concepts, mnemonic devices, definitions, diagrams, and doodles to help you understand computer science."},
+# ]
 
 # Create your views here.
 def home(request):
@@ -13,4 +14,9 @@ def about(request):
     return render(request, 'about.html')
 
 def books_index(request):
+    books = Book.objects.all()
     return render(request, 'books/index.html', {'books': books})
+
+def books_detail(request, book_id):
+    book = Book.objects.get(id=book_id)
+    return render(request, 'books/detail.html', {'book': book})

231e4141ca71a7ccc200cb973f54144f02e58bed
mlsmoorman <melissa.s.moorman@gmail.com>
2023-12-11 11:19:44 -0600

    lab pt1 complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+books = [
+    {'name': "Beginner's Step-By-Step Coding Course", 'author': 'DK', 'description': "Written by a team of expert coders and coding teachers, Beginner's Step-by-Step Coding Course is the ideal way to get to set you on the road to code."},
+    {'name': "Everything You Need to ACE Computer Science and Coding", 'author': 'Workman Publishing', 'description': "This Big Fat Notebook makes it all “sink in” with key concepts, mnemonic devices, definitions, diagrams, and doodles to help you understand computer science."},
+]
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def books_index(request):
+    return render(request, 'books/index.html', {'books': books})

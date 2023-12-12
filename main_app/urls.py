2ab66472790756baafe84e661bd6b4859321916e
mlsmoorman <melissa.s.moorman@gmail.com>
2023-12-12 10:45:52 -0600

    adds udpate, delete

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('books/', views.books_index, name='index'),
-    path('cats/<int:book_id>/', views.books_detail, name='detail'),
+    path('books/', views.BookList.as_view(), name='index'),
+    path('books/<int:book_id>/', views.books_detail, name='detail'),
+    path('books/create', views.BookCreate.as_view(), name='books_create'),
+    path('books/<int:pk>/update/', views.BookUpdate.as_view(), name='books_update'),
+    path('books/<int:pk>/delete/', views.BookDelete.as_view(), name='books_delete'),
 ]
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from .models import Book
 
-# books = [
-#     {'name': "Beginner's Step-By-Step Coding Course", 'author': 'DK', 'description': "Written by a team of expert coders and coding teachers, Beginner's Step-by-Step Coding Course is the ideal way to get to set you on the road to code."},
-#     {'name': "Everything You Need to ACE Computer Science and Coding", 'author': 'Workman Publishing', 'description': "This Big Fat Notebook makes it all “sink in” with key concepts, mnemonic devices, definitions, diagrams, and doodles to help you understand computer science."},
-# ]
+class BookList(ListView):
+    model = Book
+
+class BookCreate(CreateView):
+    model = Book
+    fields = '__all__'
+
+class BookUpdate(UpdateView):
+    model = Book
+    fields = '__all__'
+
+class BookDelete(DeleteView):
+    model = Book
+    success_url = '/books'
+
 
 # Create your views here.
 def home(request):
@@ -13,9 +27,9 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-def books_index(request):
-    books = Book.objects.all()
-    return render(request, 'books/index.html', {'books': books})
+# def books_index(request):
+#     books = Book.objects.all()
+#     return render(request, 'books/index.html', {'books': books})
 
 def books_detail(request, book_id):
     book = Book.objects.get(id=book_id)
